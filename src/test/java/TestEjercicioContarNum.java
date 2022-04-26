8bf41e957939112755d404d6c76711fa3d875672
edu.f <edu.f@com>
2022-04-26 10:14:46 +0200

    Initial commit

---
+++ src/test/java/TestEjercicioContarNum.java
@@ -0,0 +1,32 @@
+import org.example.EjercicioContarNumeros;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestEjercicioContarNum {
+    @Test
+    public void testContarNumIguales() {
+        Integer[] input= {1,1,1,1};
+        String outputEsperado = "{1=4}";
+        String outputActual = EjercicioContarNumeros.contarRepeticiones(input);
+        Assertions.assertEquals(outputEsperado, outputActual);
+    }
+
+    @Test
+    public void testContarNumNoIguales() {
+        Integer[] input= {1,2,3,4};
+        String outputEsperado = "{1=1, 2=1, 3=1, 4=1}";
+        String outputActual = EjercicioContarNumeros.contarRepeticiones(input);
+        Assertions.assertEquals(outputEsperado, outputActual);
+    }
+
+    @org.testng.annotations.Test
+    public void testContarNumerosConNull() {
+        Integer [] input= {};
+        String outputEsperado = "{}";
+        String outputActual = EjercicioContarNumeros.contarRepeticiones(input);
+        Assertions.assertEquals(outputEsperado, outputActual);
+
+    }
+
+
+}
